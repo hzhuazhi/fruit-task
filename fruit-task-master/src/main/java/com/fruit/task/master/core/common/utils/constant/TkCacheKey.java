6004abfd045210e6322c54e96b6d58efc557c97d
df-HelloWorld <duanfeng_1712@qq.com>
2020-09-24 20:48:34 +0800

    edit_3

--- fruit-task-master/src/main/java/com/fruit/task/master/core/common/utils/constant/TkCacheKey.java
+++ fruit-task-master/src/main/java/com/fruit/task/master/core/common/utils/constant/TkCacheKey.java
@@ -91,4 +91,12 @@ public interface TkCacheKey {
      */
     String LOCK_MERCHANT_RECHARGE_BALANCE = "-16";
 
+    /**
+     * LOCK-卡商充值-订单类型：下发订单
+     * <p>
+     *     只更新金额
+     * </p>
+     */
+    String LOCK_MERCHANT_RECHARGE_MONEY = "-17";
+
 }
--- fruit-task-master/src/main/java/com/fruit/task/master/core/runner/task/TaskMerchantRecharge.java
+++ fruit-task-master/src/main/java/com/fruit/task/master/core/runner/task/TaskMerchantRecharge.java
@@ -87,8 +87,6 @@ public class TaskMerchantRecharge {
                             continue;
                         }
 
-
-
                         // 正式更新卡商扩展信息的余额
                         MerchantModel merchantUpdate = TaskMethod.assembleMerchantUpdateMoney(data.getAccountId(), data.getOrderMoney());
 
@@ -173,8 +171,6 @@ public class TaskMerchantRecharge {
                             continue;
                         }
 
-
-
                         // 正式更新卡商扩展信息的余额
                         MerchantModel merchantUpdate = TaskMethod.assembleMerchantUpdateMoney(data.getAccountId(), data.getOrderMoney());
 
@@ -201,6 +197,90 @@ public class TaskMerchantRecharge {
 //                log.info("----------------------------------TaskMerchantRecharge.handleRechargeByBalance()----end");
             }catch (Exception e){
                 log.error(String.format("this TaskMerchantRecharge.handleRechargeByBalance() is error , the dataId=%s !", data.getId()));
+                e.printStackTrace();
+                // 更新此次task的状态：更新成失败：因为必填项没数据
+                StatusModel statusModel = TaskMethod.assembleTaskUpdateStatus(data.getId(), 2, 0,0, 0,0,"异常失败try!");
+                ComponentUtil.taskMerchantRechargeService.updateStatus(statusModel);
+            }
+        }
+    }
+
+
+
+    /**
+     * @Description: 处理卡商充值-订单类型等于：下发订单（只更新卡商金额）
+     * <p>
+     *     每5秒运行一次
+     *     1.查询未跑的充值信息
+     *     2.根据充值中的归属卡商ID，更新卡商的金额（这里的金额是跑量金额，更新是扣减跑量金额用于抵消）
+     * </p>
+     * @author yoko
+     * @date 2019/12/6 20:25
+     */
+    @Scheduled(fixedDelay = 5000) // 每5秒执行
+    public void handleRechargeByMoney() throws Exception{
+//        log.info("----------------------------------TaskMerchantRecharge.handleRechargeByMoney()----start");
+        // 获取订单补单数据
+        MerchantRechargeModel merchantRechargeQuery = TaskMethod.assembleMerchantRechargeByTaskQuery(limitNum, 1, 0, 3, 3, 0,
+                null,0,3, null);
+        List<MerchantRechargeModel> synchroList = ComponentUtil.taskMerchantRechargeService.getDataList(merchantRechargeQuery);
+        for (MerchantRechargeModel data : synchroList){
+            try{
+                // 锁住这个数据流水
+                String lockKey = CachedKeyUtils.getCacheKeyTask(TkCacheKey.LOCK_MERCHANT_RECHARGE_MONEY, data.getId());
+                boolean flagLock = ComponentUtil.redisIdService.lock(lockKey);
+                if (flagLock){
+                    StatusModel statusModel = null;
+
+                    // 获取卡商扩展信息
+                    MerchantModel merchantQuery = TaskMethod.assembleMerchantQuery(0, data.getAccountId(), 0);
+                    List<MerchantModel> merchantList = ComponentUtil.merchantService.findByCondition(merchantQuery);
+                    if (merchantList == null || merchantList.size() <= 0){
+                        statusModel = TaskMethod.assembleTaskUpdateStatus(data.getId(), 2, 0, 0, 0,0,"根据卡商账号ID未查到卡商扩展信息");
+                        // 更新状态
+                        ComponentUtil.taskMerchantRechargeService.updateStatus(statusModel);
+                        // 解锁
+                        ComponentUtil.redisIdService.delLock(lockKey);
+                        continue;
+                    }
+
+                    if (merchantList != null && merchantList.size() > 0){
+                        if (merchantList.size() > 1){
+                            statusModel = TaskMethod.assembleTaskUpdateStatus(data.getId(), 2, 0, 0, 0,0,"根据卡商账号ID未查到多条卡商的扩展信息");
+                            // 更新状态
+                            ComponentUtil.taskMerchantRechargeService.updateStatus(statusModel);
+                            // 解锁
+                            ComponentUtil.redisIdService.delLock(lockKey);
+                            continue;
+                        }
+
+                        // 正式更新卡商扩展信息的余额
+                        MerchantModel merchantUpdate = TaskMethod.assembleMerchantUpdateMoney(data.getAccountId(), data.getOrderMoney());
+
+                        // 锁住此卡商
+                        String lockKey_account = CachedKeyUtils.getCacheKeyTask(CacheKey.LOCK_MERCHANT_MONEY, data.getAccountId());
+                        boolean flagLock_account = ComponentUtil.redisIdService.lock(lockKey_account);
+                        if (flagLock_account){
+                            int num = ComponentUtil.merchantService.updateDeductMoney(merchantUpdate);
+                            if (num > 0){
+                                log.info("");
+                                statusModel = TaskMethod.assembleTaskUpdateStatus(data.getId(), 3, 0, 0, 0,0,null);
+                            }else{
+                                statusModel = TaskMethod.assembleTaskUpdateStatus(data.getId(), 2, 0, 0, 0,0,"更新卡商余额响应行为0");
+                            }
+                            // 解锁
+                            ComponentUtil.redisIdService.delLock(lockKey_account);
+                            // 更新状态
+                            ComponentUtil.taskMerchantRechargeService.updateStatus(statusModel);
+                        }
+                    }
+
+                    // 解锁
+                    ComponentUtil.redisIdService.delLock(lockKey);
+                }
+//                log.info("----------------------------------TaskMerchantRecharge.handleRechargeByMoney()----end");
+            }catch (Exception e){
+                log.error(String.format("this TaskMerchantRecharge.handleRechargeByMoney() is error , the dataId=%s !", data.getId()));
                 e.printStackTrace();
                 // 更新此次task的状态：更新成失败：因为必填项没数据
                 StatusModel statusModel = TaskMethod.assembleTaskUpdateStatus(data.getId(), 2, 0,0, 0,0,"异常失败try!");
